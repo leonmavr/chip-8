839e4ff6624ebe4e052eb80189d2c04c69e36801
0xleo <0xleo.git@gmail.com>
2020-12-17 22:34:58 +0100

    Pass the rom as cmd argument

--- src/demo.cpp
+++ src/demo.cpp
@@ -5,8 +5,8 @@
 int main(int argc, char *argv[])
 {
 		// test the machine
-	auto ch8 = std::make_unique<Chip8>(true);
-	ch8->loadRom("roms/tetris.c8");
+	auto ch8 = std::make_unique<Chip8>(false);
+	ch8->loadRom(argv[1]);
 	ch8->run();
 	return 0;
 }
