b08545c81a3a370e89ae02b64406da5d5c7fb5c4
0xleo <0xleo.git@gmail.com>
2020-12-21 23:40:59 +0100

    Implement pause key
    as F1

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -1,6 +1,7 @@
 #include "keyboard.hpp"
 #include <cinttypes>
 #include <SDL2/SDL.h>
+#include <iostream>
 
 
 uint8_t Keyboard::getKeyPress() {
@@ -9,6 +10,7 @@ uint8_t Keyboard::getKeyPress() {
 	const uint8_t* keys = SDL_GetKeyboardState(NULL);
 	uint8_t keyPressed = 0x0; // 4-bit Chip8 key
 	SDL_Event event;
+	int paused = 0;
 	
 	while (SDL_PollEvent(&event)) {
 		// key release - reset the keyboard
@@ -22,16 +24,25 @@ uint8_t Keyboard::getKeyPress() {
 				k = 0x0;
 		}
 
-		// keypress - turn on the pressed key
+		// keypress
 		if (event.type == SDL_KEYDOWN)  {
-			if (event.key.keysym.sym != SDLK_ESCAPE) {
+			// pause key (F1)
+			if (event.key.keysym.sym == SDLK_F1) {
+				while (paused <= 2) {
+					SDL_PollEvent(&event);
+					paused += (((event.type == SDL_KEYUP) && (paused % 2 == 0)) || ((event.type == SDL_KEYDOWN) && (paused % 2 == 1))) ;
+				}
+			}
+			// handle gameplay keys - turn on Chip-8's pressed key
+			else if (event.key.keysym.sym != SDLK_ESCAPE ) {
 				auto keyPair = m_keyQwerty2Chip8.find(event.key.keysym.sym);
 				// then unmapped key - return
 				if (keyPair == m_keyQwerty2Chip8.end()) 
 					return 0x0;
 				keyPressed = keyPair->second;
 				m_keypresses[keyPressed] = 0x1;
-			} else // exit on Esc
+			}
+			else // exit on Esc
 				std::exit(0);
 		}
 
