2ddfc94029cfeda5e55ae8bc0907bc4a13c1aee6
leonmavr <0xleo.git@gmail.com>
2024-07-22 19:06:08 +0200

    remove prints from frontend

--- include/cfg_parser.hpp
+++ include/cfg_parser.hpp
@@ -8,6 +8,7 @@
 #include <stdexcept>
 #include <cstdint>
 #include <algorithm>
+#include <unordered_map>
 
 /*
  * Description:
@@ -26,17 +27,17 @@ class CfgParser {
 public:
     CfgParser(const std::string& filename);
     int GetFrequency() const { return frequency_; }
-    const std::map<char, uint8_t>& GetKeyMap() const { return key_map_; }
+    const std::unordered_map<std::string, std::string>& GetKeyMap() const { return key_map_; }
 
 private:
     void ParseConfigFile(const std::string& filename);
     int frequency_;
-    std::map<char, uint8_t> key_map_;
+    std::unordered_map<std::string, std::string> key_map_;
     // TODO: find chip8 keyboard2keypad_ and reverse it
-    std::unordered_map<char, uint8_t> keyboard2keypad_ = {
-            {0x1: '1'}, {0x2: '2'}, {0x3: '3'}, {0xC: '4'}, {0x4: 'q'}, {0x5: 'w'},
-            {0x6: 'e'}, {0xD: 'r'}, {0x7: 'a'}, {0x8: 's'}, {0x9: 'd'}, {0xE: 'f'},
-            {0xA, 'z'}, {0x0: 'x'}, {0xB: 'c'}, {0xF, 'v'}
+    std::unordered_map<uint8_t, std::string> keyboard2keypad_ = {
+            {0x1, "1"}, {0x2, "2"}, {0x3, "3"}, {0xC, "4"}, {0x4, "q"}, {0x5, "w"},
+            {0x6, "e"}, {0xD, "r"}, {0x7, "a"}, {0x8, "s"}, {0x9, "d"}, {0xE, "f"},
+            {0xA, "z"}, {0x0, "x"}, {0xB, "c"}, {0xF, "v"}
         };
 };
 
--- src/cfg_parser.cpp
+++ src/cfg_parser.cpp
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <algorithm>
 #include <iostream>
+#include <unordered_map>
 
 /*
  * Description:
@@ -34,28 +35,24 @@ void CfgParser::ParseConfigFile(const std::string& filename) {
 
     std::string line;
     while (std::getline(file, line)) {
+        if (line.empty() || line[0] == '#' || IsWhitespaceOnly(line))
+            continue;
         // Trim whitespace from the beginning of the line
         line.erase(line.begin(), std::find_if(line.begin(), line.end(),
             [](unsigned char ch) { return !std::isspace(ch); }));
 
-        if (line.empty() || line[0] == '#' || IsWhitespaceOnly(line))
-            continue;
-
         std::istringstream iss(line);
         std::string key;
         std::string value;
-        if (std::getline(iss, key, ',') && std::getline(iss, value)) {
+        std::unordered_map<std::string, std::string> keymap;
+        if (std::getline(iss, key, ':') && std::getline(iss, value)) {
             key.erase(std::remove_if(key.begin(), key.end(), ::isspace), key.end());
             value.erase(std::remove_if(value.begin(), value.end(), ::isspace), value.end());
-
-            if (key.length() == 1 && value.length() > 2 && value.substr(0, 2) == "0x") {
-                char keyChar = key[0];
-                uint8_t hexValue = static_cast<uint8_t>(std::stoi(value, nullptr, 16));
-                key_map_[keyChar] = hexValue;
-            } 
+            uint8_t hex_key = static_cast<uint8_t>(std::stoi(key.substr(2), nullptr, 16));
+            key_map_[keyboard2keypad_[hex_key]] = value;
         } else {
             try {
-                frequency_ = std::stoi(line); // Attempt to convert the string to an integer
+                frequency_ = std::stoi(line);
             } catch (const std::invalid_argument& e) {
                 std::cerr << "Invalid argument: " << e.what() << std::endl;
             }
