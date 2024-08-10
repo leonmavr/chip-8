4d8388e14a83b1db3d3165cae8a19b406bcd5f6c
leonmavr <0xleo.git@gmail.com>
2024-08-10 11:33:43 +0200

    Add system test to ensure IBM logo is rendered correctly

--- include/chip8_tester.hpp
+++ include/chip8_tester.hpp
@@ -3,11 +3,30 @@
 
 #include "chip8.hpp"
 #include <iostream>
+#include <cstdint>
+#include <cassert>
 
 
+/**
+ * This class is responsible for testing the Chip8 class as a whole. The goal 
+ * is to verify Chip8's sanity. It does that for example by checking whether
+ * the rendered output (frame buffer) is correct.
+ */
 class Chip8Tester {
     public:
-        void foo(const Chip8& ch8)  {std::cout << ch8.PC_ << std::endl;}
+        void AssertFrameBuffer(const Chip8& ch8, const std::array<uint8_t, 64*32>& target)  {
+            for (int i = 0; i < target.size(); ++i) {
+                //std::cout << (int)ch8.frame_buffer_[i] << ", ";
+                assert(ch8.frame_buffer_ == target);
+                if (ch8.frame_buffer_[i] != target[i])
+                    std::cout << i << std::endl;
+            }
+        }
+        template <typename T>
+        void AssertPC(const Chip8& ch8, const T& target) {
+            assert(ch8.PC_ == static_cast<uint16_t>(target));
+        }
+        
     private:
 
 };
